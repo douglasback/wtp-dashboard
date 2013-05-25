119dab13c71f7f619b7abad7e22048bdf910d91f
Yahel Carmon <yahelc@gmail.com>
2013-05-25 17:43:49 -0400

    Trying to connect using connection string only

--- routes/index.js
+++ routes/index.js
@@ -14,14 +14,11 @@ module.exports = {
 		
 		var mysql = require('mysql');
 		
-		var parse  = require('url').parse;
 		var grouping = req.params.grouping === "zip" ? "zip" : "create_dt";
 		var petition_id = req.params.petition_id;
 		var signatures_by_date_sql = 'select ' + grouping +', count(*) as signatures  FROM wtp_data_signatures where petition_id=? group by 1';
-		var db_url = parse(process.env.DATABASE_URL);
-		var auth = db_url.auth.split(":");
 		
-		var client = mysql.createConnection({"host": db_url.hostname, "user": auth[0], "password": auth[1], "database": "wtpdashboard"});
+		var client = mysql.createConnection(process.env.DATABASE_URL);
 		client.query(signatures_by_date_sql, petition_id, function(er, result){
 				res.json(result);
 		});
