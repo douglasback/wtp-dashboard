4a1d0b6b251bdc9901497e0665df9f2c1c4840e5
Yahel Carmon <yahelc@gmail.com>
2013-05-25 16:06:13 -0400

    Adding MySQL package for RDS support. Including signaturesByDate route

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 // app.get('/users', user.list);
+app.get("/signaturesByDate", routes.signaturesByDate);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,22 @@ module.exports = {
             scripts: '[typeahead.js]'
             }
         );
-    }
+    },
+
+	signaturesByDate : function(req,res){
+		
+		var mysql = require('mysql');
+		
+		var parse  = require('url').parse;
+		var petition_id = req.query.petition_id;
+		var signatures_by_date_sql = 'select create_dt, count(*) as signatures  FROM wtp_data_signatures where petition_id=? group by create_dt';
+		var db_url = parse(process.env.DATABASE_URL);
+		var auth = db_url.auth.split(":");
+		
+		var client = mysql.createConnection({"host": db_url.hostname, "user": auth[0], "password": auth[1], "database": "wtpdashboard"});
+		client.query(signatures_by_date_sql, petition_id, function(er, result){
+				res.json(result);
+		});
+
+	}
 };
